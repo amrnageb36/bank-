c7b71218761daad6163c7f8168f0dadb2fc75fb7
Amr Nageb <amrnageb36@gmail.com>
2024-10-08 22:40:20 +0300

    Update bank me.cpp

--- bank me/bank me.cpp
+++ bank me/bank me.cpp
@@ -11,52 +11,7 @@
 using namespace std;
 int main()
 {
-	//Client c(1, "amrrrrrrr", "amr123456789", 50000);
-
-	//FileHelper::clearFile("client.txt", "clientLastId.txt");
-	//FileHelper::saveClient(c);
-	//FileHelper::saveClient(c2);
-	//FileHelper::saveClient(c3);
-	//FileHelper::saveLast("clientLastId.txt", 3);
-
-	//====================================================================
-
-	//Employee e1(1, "anassssss", "anassss12345", 50000);
-	//
-	//FileHelper::clearFile("employee.txt", "employeeLastId.txt");
-	//FileHelper::saveEmployee(e1);
-	//FileHelper::saveEmployee(e2);
-	//FileHelper::saveEmployee(e3);
-	//FileHelper::saveLast("employeeLastId.txt", 3);
-
-	//====================================================================
-
-
-	//Admin a1(1, "anassssss", "anassss12345", 50000);
-	//
-	//FileHelper::clearFile("admin.txt", "adminLastId.txt");
-	//FileHelper::saveAdmin(a1);
-	//FileHelper::saveAdmin(a2);
-	//FileHelper::saveAdmin(a3);
-	//FileHelper::saveLast("adminLastId.txt", 3);
-	//FileHelper::getEmployees();
-	//FileHelper::getAdmins();
-	//FileHelper::getClients();
-
-	//cout << "amins==================================\n";
-	//for (Admin a : admins) {
-	//	a.display();
-	//}
-	//cout << "amins==================================\n";
-
-	//for (Employee e : employees) {
-	//	e.display();
-	//}
-	//cout << "amins==================================\n";
-
-	///*for (Client a : clients) {
-	//a.display();
-	//}*/
+	
 	Screens::runApp();
 
 	
